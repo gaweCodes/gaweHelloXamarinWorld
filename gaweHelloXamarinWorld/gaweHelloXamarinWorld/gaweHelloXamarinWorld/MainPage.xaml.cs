1836e647fcd1858e077e878cfc5bd5e5a9c5ca1c
Gabriel Weibel <gabriel.weibel@hotmail.de>
2020-03-21 20:36:52 +0100

    Alert

--- gaweHelloXamarinWorld/gaweHelloXamarinWorld/gaweHelloXamarinWorld/MainPage.xaml.cs
+++ gaweHelloXamarinWorld/gaweHelloXamarinWorld/gaweHelloXamarinWorld/MainPage.xaml.cs
@@ -1,21 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using Xamarin.Forms;
 
 namespace gaweHelloXamarinWorld
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        private int _count;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void CountUp(object sender, EventArgs e)
+        {
+            if (_count == 0)
+                await DisplayAlert("Button pressed", "You pressed the button and executed an increase.", "Ok");
+            Interlocked.Increment(ref _count);
+            ((Button)sender).Text = $"You clicked {_count} times.";
         }
     }
 }
